2dda7afc638d00b5e0f660725cefa63ce0f18ace
Eric Macy <eric_macy@nonik.com>
2020-02-29 10:51:08 -0500

    Dropped bottom shot by 10%

--- Robot/src/main/java/frc/robot/Robot.java
+++ Robot/src/main/java/frc/robot/Robot.java
@@ -204,7 +204,7 @@ public class Robot extends TimedRobot {
     /* Shoot High - Button 2 (B) */
     if (controller_0.getRawButton(2) || controller_1.getRawButton(2)) {
       top_motor.set(0.75);
-      bottom_motor.set(-0.80);
+      bottom_motor.set(-0.70);
       ball_ramp.set(DoubleSolenoid.Value.kForward);
     }
 
